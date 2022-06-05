a29c39118d672fe3594b5dbfb0d2aefa09e8a174
Steven Wu <stevenhwu@users.noreply.github.com>
2022-06-05 21:55:21 +0800

    Add code to fetch online database
    - This function is not called from the main.py at all
    - Might need to change URL later

--- python/setup.py
+++ python/setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='MicroFisher',
       version='0.5',
-      description='MicroFisher pipeline',
-      packages=['MicroFisher'])
+          description='MicroFisher pipeline',
+      #packages=['MicroFisher'])
+      url='https://github.com/NFREC-Liao-Lab/MicroFisher-Fungal-Profiling/',
+      packages=['.']
+     )
